718f323093983f16e118ba812f6e135b24177753
Luiz Rodrigues <luizrodriguescode@gmail.com>
2025-12-29 10:24:58 -0300

    feat: Implementação do nikto basico

--- modules/http.sh
+++ modules/http.sh
@@ -7,15 +7,12 @@ WORKDIR="workspaces/$target/$service_name"
 run_whatweb(){
 	local target="$1"
 	local port="$2"
-	local service_name="$3"
-	echo "$target"
-	echo "$port"
-	echo "$target:$port"
+
 	if command -v whatweb > /dev/null
 	then
 		whatweb -a 3 "http://$target:$port" > "$WORKDIR/whatweb.txt"
 	else
-		echo "[!] ERRO: what web não esta instalado"
+		echo "[!] ERRO: whatweb não esta instalado"
 	fi
 
 	if [ -s  "$WORKDIR/whatweb.txt" ]
@@ -26,5 +23,28 @@ run_whatweb(){
 		return 0
 	fi
 }
+run_nikto(){
+	local target="$1"
+        local port="$2"
+	
+	if command -v nikto > /dev/null
+	then
+		nikto -h "$target" -p "$port" -o "$WORKDIR/nikto.html" -Format htm
+	else
+		echo "ERRO: nikto nao instalado"
+	fi
+	if [ -s "$WORKDIR/nikto.html"  ]
+	then
+		echo "[*] Resultado do nikto foi salvo"
+		return 0
+	else
+		echo "[!] Resultado do nikto nao foi salvo"
+		return 1
+	fi
+}
 
-run_whatweb "$target" "$port" "$service_name"
+
+
+run_whatweb "$target" "$port" 
+run_nikto "$target" "$port" 
+
